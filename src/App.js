271badbf7afa9daf9db7295a5dee613bb3a2ef25
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-10-19 21:25:31 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import React from  "react";
+import Logo from "./Logo";
+import BestPokemon from "./BestPokemon";
+import CaughtPokemon from "./CaughtPokemon";
+// import PokemonMoves from "./PokemonMoves";
+import PokemonMovesSelector from "./PokemonMovesSelector";
+import PokemonCity from "./PokemonCity";
 
-function App() {
+const appName = "Gulnihal's Pokedex";
+const abilities = ["Anticipation", "Adaptability", "Run-Away"];
+const date = new Date().toLocaleDateString();
+const App = () => {
+  function logWhenClicked() {
+    console.log("Button was clicked!");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Logo appName = {appName} handleClick={logWhenClicked}/>
+      <BestPokemon abilities = {abilities}/>
+      <CaughtPokemon date ={date}/>
+      <PokemonMovesSelector />
+      <PokemonCity />
     </div>
   );
-}
-
+};
 export default App;
